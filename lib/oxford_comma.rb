6c354166007ccfc76136fb26bf5e4c025cb99f24
Robert Quintana <robertquintanatx@gmail.com>
2020-05-28 07:06:19 +0000

    Automatically backed up by Learn

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,7 +1,9 @@
 def oxford_comma(array)
   if array.count == 1
     array.join
-  elsif array.count > 1
+  elsif array.count == 2
     array.join(" and ")
+  elsif array.count == 3
+    
   end
 end
